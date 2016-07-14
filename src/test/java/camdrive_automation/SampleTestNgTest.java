a8666ae1653a7b4263871d3ad6c40d0c0bdad248
IskoAnton <isko.anton@gmail.com>
2016-07-14 13:45:58 +0700

    Создан проект из архетипа

---
+++ src/test/java/camdrive_automation/SampleTestNgTest.java
@@ -0,0 +1,25 @@
+package camdrive_automation;
+
+import org.openqa.selenium.support.PageFactory;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import camdrive_automation.pages.HomePage;
+
+public class SampleTestNgTest extends TestNgTestBase {
+
+  private HomePage homepage;
+
+  @BeforeMethod
+  public void initPageObjects() {
+    homepage = PageFactory.initElements(driver, HomePage.class);
+  }
+
+  @Test
+  public void testHomePageHasAHeader() {
+    driver.get(baseUrl);
+    Assert.assertFalse("".equals(homepage.header.getText()));
+  }
+}
